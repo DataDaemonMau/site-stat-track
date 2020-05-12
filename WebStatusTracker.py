83326e36c81406287262a07f4478e1b2348da65c
Mauricio Guzman <59806578+DataDaemonMau@users.noreply.github.com>
2020-05-12 11:06:59 -0700

    Update WebStatusTracker.py

--- WebStatusTracker.py
+++ WebStatusTracker.py
@@ -4,16 +4,28 @@
     online, offline..etc.
 '''
 # modules to import here
+import sys
+import requests
 
+#def main():
 
-# Inputs Workflow
-# Py queries the user for a url
-# user enters a site url
+    # Inputs Workflow
+print("Welcome to your Web Status Tracker tool..")
+    # Py queries the user for a url
+url = input('Enter your name: ')
+try:
+    r = requests.head(url)
+    print(r.status_code)
+except requests.ConnectionError:
+    print("failed to connect")
+    # user enters a site url
 
-# Processing Workflow
-# Py takes url & searches web for url
-# obtains site status code
+    # Processing Workflow
+    #print(urllib.request.urlopen(url).getcode())
+    # Py takes url & searches web for url
+    # obtains site status code
 
-# Outputs Workflow
-# Py outputs the status code to the user
-
+    # Outputs Workflow
+    # Py outputs the status code to the user
+    #sys.exit()
+#main()
