df293e7737a8bac04e19746733e287e1de10c259
Mauricio Guzman <59806578+DataDaemonMau@users.noreply.github.com>
2020-05-12 08:44:44 -0700

    Update WebStatusTracker.py

--- WebStatusTracker.py
+++ WebStatusTracker.py
@@ -1,1 +1,19 @@
+'''Program: Website Status Tracker
+    Author: DataDaemonMau
+    Purpose: Allows the user to check a site(s) status, whether they are
+    online, offline..etc.
+'''
+# modules to import here
+
+
+# Inputs Workflow
+# Py queries the user for a url
+# user enters a site url
+
+# Processing Workflow
+# Py takes url & searches web for url
+# obtains site status code
+
+# Outputs Workflow
+# Py outputs the status code to the user
 
